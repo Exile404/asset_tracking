92f62c53ba49020b329c04c72984e949f63d5e78
Exile404 <tahsinul.haque.dhrubo@gmail.com>
2024-03-04 15:19:37 +0600

    fix: test case error

--- employees/tests.py
+++ employees/tests.py
@@ -3,8 +3,8 @@ from .models import Employee
 
 class EmployeeModelTest(TestCase):
     def setUp(self):
-        Employee.objects.create(name='John Doe', email='john@example.com', department='IT')
+        Employee.objects.create(name='Test1', email='test1@example.com', department='IT')
 
     def test_employee_str_representation(self):
-        john = Employee.objects.get(name='John Doe')
-        self.assertEqual(str(john), 'John Doe')
+        john = Employee.objects.get(name='Test1')
+        self.assertEqual(str(john), 'Test1')
